06981eefb30fc26f0d17016eefc67ad703217a3c
pengyongtxd <1626897120@qq.com>
2019-12-12 15:30:00 +0800

    修改请求

--- h5view/src/http/api.js
+++ h5view/src/http/api.js
@@ -5,18 +5,18 @@ import { get } from "./http";
  * @param {Number} p
  * storeId:门店ID
  */
-export const apiCardInfo = p => get(`api/card/info/${p}`);
+export const apiCardInfo = p => get(`screen/card/info/${p}`);
 
 /**
  * 门店微信群数据
  * @param {Number} p
  * storeId:门店ID
  */
-export const apiGroupInfo = p => get(`api/group/info/${p}`);
+export const apiGroupInfo = p => get(`screen/group/info/${p}`);
 
 /**
  * 登录
  * @param {Number} p
  * id:门店ID
  */
-export const apiLogin = p => get(`api/login/${p}`);
+export const apiLogin = p => get(`screen/login/${p}`);
