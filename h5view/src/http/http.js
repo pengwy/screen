6b5d5c91a1108fe44fa8f614ab80031f8a7c09a1
pengyongtxd <1626897120@qq.com>
2019-12-12 15:59:13 +0800

    修改环境变量

--- h5view/src/http/http.js
+++ h5view/src/http/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.cw100.com/";
+axios.defaults.baseURL = "https://api.cw100.cn/";
 /**
  * get方法，对应get请求
  * @param {String} url [请求的url地址]
