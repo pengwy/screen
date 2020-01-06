252556e9e8b272a027bd4ce36b7c43aa1b287d2f
pengyongtxd <1626897120@qq.com>
2020-01-06 13:37:48 +0800

    原生生成app

---
+++ AndroidToH5/app/src/main/java/com/example/administrator/androidtoh5/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.administrator.androidtoh5
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by 舍长
+ * describe:Android与H5混合开发「kotlin，WebView」
+ * 地址:https://www.jianshu.com/p/4365d99508e4
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+    }
+}
---
+++ AndroidToH5/app/src/main/java/com/example/administrator/androidtoh5/util/L.kt
@@ -0,0 +1,16 @@
+package com.example.administrator.androidtoh5.util
+
+import android.util.Log
+
+/**
+ * Created by 舍长 on 2018/7/1.
+ * 舍长:
+ */
+object L {
+    //    TAG
+    public var TAG: String = "tonJies"
+
+    fun d(test: String) {
+        Log.d(TAG, test)
+    }
+}
